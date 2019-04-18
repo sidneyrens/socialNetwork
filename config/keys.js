233cb47d68c1b014467cb0bec8a2853c55ff2e2a
sidneyrens <sidneyrens@gmail.com>
2019-04-18 12:08:44 -0500

    added tokens for authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://sidney:sidney!234@cluster0-p7zbn.mongodb.net/test?'
+  mongoURI: 'mongodb+srv://sidney:sidney!234@cluster0-p7zbn.mongodb.net/test?',
+  secretOrKey: 'secret'
 }
