a0cfa11abcf12467f4bdaa0b61f4255d6f52e8a1
sidneyrens <sidneyrens@gmail.com>
2019-04-22 15:21:29 -0500

    added validation for profile inputs and created profile schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport')
 
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
+
 // Load User model
 var User = require('../../models/User');
 const keys = require('../../config/keys');
@@ -20,9 +24,17 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists' });
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: '200', // size
@@ -54,6 +66,13 @@ router.post('/register', (req, res) => {
 // @desc    Login User / Returning JWT Token
 // @access  Public
 router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -61,13 +80,13 @@ router.post('/login', (req, res) => {
   User.findOne({ email }).then(user => {
     //check for user
     if (!user) {
-      return res.status(404).json({ email: 'User not found' });
+      errors.email = 'User not found';
+      return res.status(404).json(errors);
     }
 
     //check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        // return res.json({msg: 'Success'})
         //user matched
         const payload = {
           id: user.id,
@@ -83,7 +102,8 @@ router.post('/login', (req, res) => {
           });
         });
       } else {
-        return res.status(400).json({ password: 'Password Incorrect' });
+        errors.password = 'Password incorrect'
+        return res.status(400).json(errors);
       }
     });
   });
